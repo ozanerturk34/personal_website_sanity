d1cd6ae71626fa39110caec9f999b0f860522bc5
Ozan Ertürk <serturk@flexport.com>
2023-07-22 16:21:19 +0300

    Add slug to project

--- schemas/project.ts
+++ schemas/project.ts
@@ -12,6 +12,16 @@ export const projectSchema = defineType({
       validation: (Rule: any) => Rule.required(),
     }),
     defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    }),
+    defineField({
       name: 'description',
       title: 'Description',
       type: 'string',
